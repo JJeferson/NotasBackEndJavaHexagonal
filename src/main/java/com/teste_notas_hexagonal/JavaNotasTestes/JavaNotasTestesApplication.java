2f2bbf9979ec074fd4693236b4458f9bab949fcb
JJeferson <48735241+JJeferson@users.noreply.github.com>
2021-09-02 12:55:51 -0300

    Commit 0 Inicio do Projeto

---
+++ src/main/java/com/teste_notas_hexagonal/JavaNotasTestes/JavaNotasTestesApplication.java
@@ -0,0 +1,13 @@
+package com.teste_notas_hexagonal.JavaNotasTestes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaNotasTestesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaNotasTestesApplication.class, args);
+	}
+
+}
